b73b37094d58ceb5388c1ebd6bb7168fdd06d5b0
VAIBHAV GOEL <vaibhav.goel_cs.aiml19@gla.ac.in>
2024-11-06 18:41:54 +0000

    Finish permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 from blog.api.serializers import PostSerializer
 from blog.models import Post
 
@@ -10,5 +11,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
