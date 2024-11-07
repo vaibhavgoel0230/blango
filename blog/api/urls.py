61fd2448cdc09350877d4538f120475fb66b9d2f
VAIBHAV GOEL <vaibhav.goel_cs.aiml19@gla.ac.in>
2024-11-07 08:43:43 +0000

    Finish related fields

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 from rest_framework.authtoken import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from blog.api.views import PostList, PostDetail
+from blog.api.views import PostList, PostDetail, UserDetail
 
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
 ]
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,7 +1,8 @@
 from rest_framework import generics
 
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
-from blog.api.serializers import PostSerializer
+from blog.api.serializers import PostSerializer, UserSerializer
+from blango_auth.models import User
 from blog.models import Post
 
 
@@ -14,3 +15,8 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
