724ee633296fe7558832aaf6ade7ee75a9b75dc4
VAIBHAV GOEL <vaibhav.goel_cs.aiml19@gla.ac.in>
2024-11-11 18:27:37 +0000

    Finish JavaScript classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -36,30 +36,81 @@
 
 // console.log('After setTimeout')
 
-for(let i = 0; i < 10; i += 1) {
-  console.log('for loop i: ' + i)
+// for(let i = 0; i < 10; i += 1) {
+//   console.log('for loop i: ' + i)
+// }
+
+// let j = 0
+// while(j < 10) {
+//   console.log('while loop j: ' + j)
+//   j += 1
+// }
+
+// let k = 10
+
+// do {
+//   console.log('do while k: ' + k)
+// } while(k < 10)
+
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// numbers.forEach((value => {
+//   console.log('For each value ' + value)
+// }))
+
+// const doubled = numbers.map(value => value * 2)
+
+// console.log('Here are the doubled numbers')
+
+// console.log(doubled)
+
+class Greeter{
+  constructor (name){
+    this.name = name
+  }
+
+  getGreeting (){
+    if( this.name === undefined ){
+      return 'Hello, no name'
+    }
+
+    return 'Hello, ' + this.name
+  }
+
+  showGreeting (greetingMessage){
+    console.log(greetingMessage)
+  }
+
+  greet(){
+    this.showGreeting(this.getGreeting())
+  }
 }
 
-let j = 0
-while(j < 10) {
-  console.log('while loop j: ' + j)
-  j += 1
+class DelayedGreeter extends Greeter{
+  delay = 2000
+
+  constructor (name, delay){
+    super(name)
+    if( delay !== undefined ){
+      this.delay = delay
+    }
+  }
+
+  greet (){
+    setTimeout(
+      () => {
+        this.showGreeting(this.getGreeting())
+      }, this.delay
+    )
+  }
 }
 
-let k = 10
 
-do {
-  console.log('do while k: ' + k)
-} while(k < 10)
+const g = new Greeter('Vaibhav')
+g.greet()
 
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const dg2 = new DelayedGreeter('Patchy 2 Seconds')
+dg2.greet()
 
-numbers.forEach((value => {
-  console.log('For each value ' + value)
-}))
-
-const doubled = numbers.map(value => value * 2)
-
-console.log('Here are the doubled numbers')
-
-console.log(doubled)
+const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
+dg1.greet()
