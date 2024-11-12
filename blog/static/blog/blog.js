0cb34b96fbada768ac901ba67f76ff0ffc31b3c7
VAIBHAV GOEL <vaibhav.goel_cs.aiml19@gla.ac.in>
2024-11-12 11:13:09 +0000

    Finish fetch and React hooks

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -59,24 +59,31 @@ class PostRow extends React.Component {
 
 class PostTable extends React.Component {
   state = {
-    dataLoaded: true,
-    data: {
-      results: [
-        {
-          id: 15,
-          tags: [
-            'django', 'react'
-          ],
-          'hero_image': {
-            'thumbnail': '/media/__sized__/hero_images/samurai-thumbnail-100x100.png',
-            'full_size': '/media/hero_images/samurai.png'
-          },
-          title: 'Test Post',
-          slug: 'test-post',
-          summary: 'A test post, created for Django/React.'
+    dataLoaded: false,
+    data: null
+  }
+
+  componentDidMount () {
+    fetch(this.props.url).then(response => {
+      if(response.status !== 200){
+        throw new Error('Invalid status from server: ' + response.statusText)
+      }
+
+      return response.json()
+    }).then(data => {
+      this.setState({
+        dataLoaded: true,
+        data: data
+      })
+    }).catch(e => {
+      console.error(e)
+      this.setState({
+        dataLoaded: true,
+        data: {
+          results: []
         }
-      ]
-    }
+      })
+    })
   }
 
   render(){
@@ -115,6 +122,6 @@ class PostTable extends React.Component {
 
 const domContainer = document.getElementById('react_root')
 ReactDOM.render(
-  React.createElement(PostTable),
+  React.createElement(PostTable, {url: postListUrl}),
   domContainer
 )
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ import logging
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.urls import reverse
 
 from blog.forms import CommentForm
 from blog.models import Post
@@ -42,4 +43,4 @@ def get_ip(request):
   return HttpResponse(request.META['REMOTE_ADDR'])
 
 def post_table(request):
-    return render(request, "blog/post-table.html")
+    return render(request, "blog/post-table.html", {"post_list_url": reverse("post-list")})
